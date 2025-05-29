9df91f7890b972d217d3d4953ad0e0a69724afd0
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2025-05-29 17:47:24 -0400

    Loading overlay on navigate

---
+++ capacitor/src/lib/LoadingOverlay.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { quadOut } from "svelte/easing";
+    import { fade } from "svelte/transition";
+</script>
+
+<div class="overlay" transition:fade={{duration: 200, easing: quadOut}}>
+    <span class="spinner"></span>
+</div>
+
+<style>
+    .overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(255, 255, 255, 0.8);
+        display: grid;
+        place-items: center;
+    }
+
+    .spinner {
+        border: 5px solid var(--accent);
+        border-bottom-color: transparent;
+        border-radius: 50%;
+        width: 48px;
+        height: 48px;
+        animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin {
+        to {
+            transform: rotate(360deg);
+        }
+    }
+</style>
---
+++ capacitor/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { navigating } from "$app/state";
+    import LoadingOverlay from "$lib/LoadingOverlay.svelte";
+
+    const { children } = $props();
+</script>
+
+{@render children()}
+
+{#if navigating.to}
+    <LoadingOverlay />
+{/if}
