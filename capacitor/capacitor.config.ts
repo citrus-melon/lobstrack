97f15490179b8d51425a85b51eb52a2bd475e786
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2025-05-29 01:20:37 -0400

    System bar/safe area stupidity

--- capacitor/capacitor.config.ts
+++ capacitor/capacitor.config.ts
@@ -1,9 +1,20 @@
+/// <reference types="@capacitor/status-bar" />
+/// <reference types="@capacitor/keyboard" />
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.lobstahbots.lobstrack',
   appName: 'lobstrack',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    StatusBar: {
+      style: 'LIGHT',
+    },
+    Keyboard: {
+      resizeOnFullScreen: true,
+    }
+  }
 };
 
 export default config;
