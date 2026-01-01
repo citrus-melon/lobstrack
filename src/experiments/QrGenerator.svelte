718e2b3eb62b8f46af38743983d172bf96f08ec9
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2026-01-01 17:51:19 -0500

    Cleap up/extract QR generator algorithm from demo

--- src/experiments/QrGenerator.svelte
+++ src/experiments/QrGenerator.svelte
@@ -1,103 +1,20 @@
+<input type="url" bind:value={baseUrl} placeholder="Base URL" />
+<input type="number" bind:value={startId} min="0" placeholder="Starting ID" />
 <input type="file" name="template" accept=".svg,image/svg+xml" bind:files={templateFiles}>
 <button onclick={generate} disabled={!templateFiles}>Generate QR Codes</button>
-<iframe srcdoc={iframeContent} bind:this={iframe} sandbox="allow-modals" width="100%" height="500px" title="print"></iframe>
+<iframe srcdoc={iframeContent} bind:this={iframe} sandbox="allow-modals allow-scripts" width="100%" height="500px" title="print"></iframe>
 
 <script lang="ts">
-    import QRCode from "qrcode";
     import { sqids } from '$lib/sqids';
+    import { parseTemplate, populateTemplate } from '$lib/qrGenerator';
+    import { start } from 'repl';
 
+    let baseUrl: string = location.origin;
+    let startId: number = 0;
     let templateFiles: FileList | null = null;
-
     let iframeContent: string = "";
-
     let iframe: HTMLIFrameElement;
 
-    type Template = {
-        templateDocument: Document;
-        itemCount: number;
-    }
-
-    async function parseTemplate(templateString: string): Promise<Template> {
-        const templateDocument = new DOMParser().parseFromString(templateString, "image/svg+xml");
-
-        const itemElements = templateDocument.querySelectorAll("[*|label='item']");
-
-        if (itemElements.length === 0) {
-            templateDocument.documentElement.setAttribute("label", "item");
-
-            if (templateDocument.querySelectorAll("[*|label='qr']").length > 1) {
-                throw new Error("Multiple QR code placeholders found in a single-item template");
-            }
-
-            return { templateDocument, itemCount: 1 };
-        } else {
-            for (const itemElement of itemElements) {
-                if (itemElement.querySelector("[*|label='item']")) {
-                    throw new Error("Nested item groups are not allowed");
-                }
-            }
-
-            return { templateDocument, itemCount: itemElements.length };
-        }
-    }
-
-    function generateUrl(sqid: string) {
-        return `${location.origin}/qr/${sqid}`;
-    }
-
-    async function populateTemplate(template: Template, itemSqids: string[]) {
-        const svgDocument = template.templateDocument.cloneNode(true) as Document;
-        const itemElements = svgDocument.querySelectorAll("[*|label='item']");
-
-        if (itemElements.length !== itemSqids.length) {
-            throw new Error("Item count does not match the number of provided item sqids");
-        }
-
-        for (const itemElement of itemElements) {
-            const itemSqid = itemSqids.shift()!;
-
-            const qrPlaceholders = itemElement.querySelectorAll("[*|label='qr']");
-            for (const qrPlaceholder of qrPlaceholders) {
-                if (qrPlaceholder instanceof SVGElement === false) {
-                    throw new Error("QR code placeholder is not an SVG element");
-                }
-                const x = qrPlaceholder.getAttribute("x");
-                const y = qrPlaceholder.getAttribute("y");
-                const width = qrPlaceholder.getAttribute("width");
-                const height = qrPlaceholder.getAttribute("height");
-                const stroke = qrPlaceholder.style.stroke.match(/\d+/g)?.map(v => Number(v).toString(16).padStart(2, "0")).join("");
-
-                // generate QR code image
-                const qrCodeSvg = await QRCode.toString(generateUrl(itemSqid), { 
-                    type: "svg",
-                    margin: 0,
-                    color: {
-                        dark: stroke || undefined,
-                        light: "#00000000" // transparent background
-                    }
-                });
-
-                const qrCodeElement = new DOMParser().parseFromString(qrCodeSvg, "image/svg+xml").documentElement;
-
-                if (x) qrCodeElement.setAttribute("x", x);
-                if (y) qrCodeElement.setAttribute("y", y);
-                if (width) qrCodeElement.setAttribute("width", width);
-                if (height) qrCodeElement.setAttribute("height", height);
-                
-                // replace rectangle with QR code image
-
-                qrPlaceholder.replaceWith(qrCodeElement);
-            }
-            
-            const textElements = itemElement.querySelectorAll("text");
-            for (const textElement of textElements) {
-                textElement.innerHTML = textElement.innerHTML.replace("{url}", generateUrl(itemSqid));
-                textElement.innerHTML = textElement.innerHTML.replace("{code}", itemSqid);
-            }
-        }
-
-        return svgDocument.documentElement.outerHTML;
-    }
 
     async function generate() {
         try {
@@ -108,14 +25,14 @@
     
             const itemSqids: string[] = [];
             for (let i = 0; i < template.itemCount; i++) {
-                itemSqids.push(sqids.encode([i]));
+                itemSqids.push(sqids.encode([startId + i]));
             }
-            const populatedTemplate = await populateTemplate(template, itemSqids);
+
+            const populatedTemplate = await populateTemplate(template, itemSqids, new URL(baseUrl));
     
             iframeContent = populatedTemplate;
-    
-            // iframeContent = await populateTemplate(templateString);
-            iframe.contentWindow?.print();
+            startId += template.itemCount;
+            // iframe.contentWindow?.print();
         } catch (e: any) {
             alert(e.message || "Failed to generate QR codes.");
         }
---
+++ src/lib/qrGenerator.ts
@@ -0,0 +1,92 @@
+import QRCode from "qrcode";
+
+type Template = {
+    templateDocument: Document;
+    itemCount: number;
+}
+
+export async function parseTemplate(templateString: string): Promise<Template> {
+    const templateDocument = new DOMParser().parseFromString(templateString, "image/svg+xml");
+
+    const itemElements = templateDocument.querySelectorAll("[*|label='item']");
+
+    if (itemElements.length === 0) {
+        templateDocument.documentElement.setAttribute("label", "item");
+
+        if (templateDocument.querySelectorAll("rect[*|label='qr']").length > 1) {
+            throw new Error("Invalid template: Single-item template (no groups representing items) cannot contain multiple QR code placeholders");
+        }
+
+        return { templateDocument, itemCount: 1 };
+    } else {
+        for (const itemElement of itemElements) {
+            if (itemElement.querySelector("[*|label='item']")) {
+                throw new Error("Invalid template: groups representing items cannot be nested");
+            }
+        }
+
+        return { templateDocument, itemCount: itemElements.length };
+    }
+}
+
+function generateUrl(sqid: string, base: URL) {
+    return `${base.origin}/qr/${sqid}`;
+}
+
+function rgbToHex(rgb: string) {
+    return rgb
+        .match(/\d+/g)
+        ?.map(v => Number(v).toString(16).padStart(2, "0"))
+        .join("");
+}
+
+export async function populateTemplate(template: Template, itemSqids: string[], baseURL: URL) {
+    const svgDocument = template.templateDocument.cloneNode(true) as Document;
+    const itemElements = svgDocument.querySelectorAll("[*|label='item']");
+
+    if (itemElements.length !== itemSqids.length) {
+        throw new Error("Template item count does not match generated item count");
+    }
+
+    for (const itemElement of itemElements) {
+        const itemSqid = itemSqids.shift()!;
+
+        const qrPlaceholders = itemElement.querySelectorAll("[rect|label='qr']");
+        for (const qrPlaceholder of qrPlaceholders) {
+            // Extract attributes from placeholder
+            if (qrPlaceholder instanceof SVGElement === false) throw new Error("QR code placeholder is not an SVG element");
+            const x = qrPlaceholder.getAttribute("x");
+            const y = qrPlaceholder.getAttribute("y");
+            const width = qrPlaceholder.getAttribute("width");
+            const height = qrPlaceholder.getAttribute("height");
+            const strokeHex = rgbToHex(qrPlaceholder.style.stroke);
+
+            // generate QR code image
+            const qrCodeSvg = await QRCode.toString(generateUrl(itemSqid, baseURL), { 
+                type: "svg",
+                margin: 0,
+                color: {
+                    dark: strokeHex || undefined,
+                    light: "#00000000" // transparent background
+                }
+            });
+
+            const qrCodeElement = new DOMParser().parseFromString(qrCodeSvg, "image/svg+xml").documentElement;
+
+            if (x) qrCodeElement.setAttribute("x", x);
+            if (y) qrCodeElement.setAttribute("y", y);
+            if (width) qrCodeElement.setAttribute("width", width);
+            if (height) qrCodeElement.setAttribute("height", height);
+
+            qrPlaceholder.replaceWith(qrCodeElement);
+        }
+        
+        const textElements = itemElement.querySelectorAll("text");
+        for (const textElement of textElements) {
+            textElement.innerHTML = textElement.innerHTML.replace("{url}", generateUrl(itemSqid, baseURL));
+            textElement.innerHTML = textElement.innerHTML.replace("{code}", itemSqid);
+        }
+    }
+
+    return svgDocument.documentElement.outerHTML;
+}
